0652c5da91ef46bdf1871af80c9fc5769d39d3dd
michaeliyaomolere <mikoa04@gmail.com>
2019-04-16 19:02:50 -0400

    Building The App

--- ATM Machine.py
+++ ATM Machine.py
@@ -9,3 +9,13 @@ pins = ["1234", "2222", "3333"]
 amounts = [1000, 2000, 3000]
 count = 0
 
+#  Using The While Loop To Check For The Presence Of Username Inputed
+
+while True:
+    user = input("\n ENTER USER NAME")
+    user = user.lower()
+    if user in users:
+        if user == user[0]:
+            n = 0
+        elif user == users[1]:
+            n = 1
