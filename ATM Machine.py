6f90131c497d0e682070b38058ee8d4e301d4612
michaeliyaomolere <mikoa04@gmail.com>
2019-04-16 18:56:43 -0400

    Building The App

--- ATM Machine.py
+++ ATM Machine.py
@@ -1,3 +1,11 @@
 import getpass
 import string
 import os
+
+#  Creating The List Of Users, Pin And Account Statement
+
+users = ["user", "user2", "user3"]
+pins = ["1234", "2222", "3333"]
+amounts = [1000, 2000, 3000]
+count = 0
+
