c0c9f214c86641d597643c8351bc497d2b6de386
michaeliyaomolere <mikoa04@gmail.com>
2019-04-20 09:31:01 -0400

    Building The App

--- ATM Machine.py
+++ ATM Machine.py
@@ -23,81 +23,81 @@ while True:
             n = 2
         break
     else:
-        print('----------------')
-        print('****************')
-        print('INVALID USERNAME')
-        print('****************')
-        print('----------------')
+        print("----------------")
+        print("****************")
+        print("INVALID USERNAME")
+        print("****************")
+        print("----------------")
 
 # Checking the validity of the pin entered
 while count < 3:
-    print('------------------')
-    print('******************')
+    print("------------------")
+    print("******************")
     pin = str(getpass.getpass("PLEASE ENTER YOUR PIN"))
-    print('******************')
-    print('------------------')
+    print("******************")
+    print("------------------")
     if pin.isdigit():
         if user == "user1":
             if pin == pins[0]:
                 break
             else:
                 count += 1
-                print('-----------')
-                print('***********')
-                print('INVALID PIN')
-                print('***********')
-                print('-----------')
+                print("-----------")
+                print("***********")
+                print("INVALID PIN")
+                print("***********")
+                print("-----------")
                 print()
 
         if user == "user2":
             if pin == pins[1]:
                 break
             else:
-                print('-----------')
-                print('***********')
-                print('INVALID PIN')
-                print('***********')
-                print('-----------')
+                print("-----------")
+                print("***********")
+                print("INVALID PIN")
+                print("***********")
+                print("-----------")
                 print()
 
         if user == "user3":
             if pin == pin[2]:
                 break
             else:
-                print('-----------')
-                print('***********')
-                print('INVALID PIN')
-                print('***********')
-                print('-----------')
+                print("-----------")
+                print("***********")
+                print("INVALID PIN")
+                print("***********")
+                print("-----------")
                 print()
     else:
-        print('------------------------')
-        print('************************')
-        print('PIN CONSISTS OF 4 DIGITS')
-        print('************************')
-        print('------------------------')
+        print("------------------------")
+        print("************************")
+        print("PIN CONSISTS OF 4 DIGITS")
+        print("************************")
+        print("------------------------")
         count += 1
 
 # If The Pin Entered 3 Times Is Invalid
 if count == 3:
-    print('-----------------------------------')
-    print('***********************************')
-    print('3 UNSUCCESFUL PIN ATTEMPTS, EXITING')
-    print('!!!!!YOUR CARD HAS BEEN LOCKED!!!!!')
-    print('***********************************')
-    print('-----------------------------------')
+    print("-----------------------------------")
+    print("***********************************")
+    print("3 UNSUCCESFUL PIN ATTEMPTS, EXITING")
+    print("!!!!!YOUR CARD HAS BEEN LOCKED!!!!!")
+    print("***********************************")
+    print("-----------------------------------")
     exit()
 
-print('-------------------------')
-print('*************************')
-print('LOGIN SUCCESSFUL, CONTINUE')
-print('*************************')
-print('-------------------------')
+print("-------------------------")
+print("*************************")
+print("LOGIN SUCCESSFUL, CONTINUE")
+print("*************************")
+print("-------------------------")
 print()
-print('--------------------------')
-print('**************************')
+print("--------------------------")
+print("**************************")
 print(str.capitalize(users[n]), "Welcome to ATM BANK")
-print('**************************')
+print("**************************")
 
 
 
